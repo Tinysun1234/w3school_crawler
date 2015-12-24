2277a6b670ddf499c24fc66e1eb65c56693de0c7
Tinysun1234 <tisun1017@126.com>
2015-12-24 16:30:16 +0800

    First fix

---
+++ mytest_proj/pack2/mod2.py
@@ -0,0 +1,17 @@
+'''
+Created on Dec 24, 2015
+
+@author: tisun
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
